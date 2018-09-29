31653364265ba4f94cfc1cc85f874309aaa1a97f
xlopec23 <xlopec23@tut.by>
2018-09-29 21:49:48 +0300

    Yra!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  zerosCount=0;
+      do {
+    number = number/ 5>>0;
+    zerosCount += number;
+    }
+     while (number > 1);
+
+  return (zerosCount);
 }
